6fc1ccc38f227fa99894f4cc0e7229b4f087d6d9
Amarnath <amarnathreddy841@gmail.com>
2025-12-19 23:12:26 +0530

    Load db config from env variables

--- config/db.go
+++ config/db.go
@@ -2,22 +2,26 @@ package config
 
 import (
 	"context"
-	"log"
+	"fmt"
+	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func ConnectDB() *pgxpool.Pool {
-	dsn := "postgres://appuser:password@localhost:5432/userdb?sslmode=disable"
+	connStr := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
 
-	pool, err := pgxpool.New(context.Background(), dsn)
+	db, err := pgxpool.New(context.Background(), connStr)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	if err := pool.Ping(context.Background()); err != nil {
-		log.Fatal(err)
-	}
-
-	return pool
+	return db
 }
